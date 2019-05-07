82ee71deab0aa811e5e0e78a1bde7140b9f3d6b5
Jaime Barbosa <jaime.barbosa@solucaosistemas.net>
2019-05-07 08:04:14 -0300

    Implementado tela de cadastro de Usuario.

--- src/app/components/_seguranca/usuario/usuario-form.component.ts
+++ src/app/components/_seguranca/usuario/usuario-form.component.ts
@@ -1,5 +1,9 @@
+import { MensagemService } from 'src/app/services/mensagem.service';
+import { Usuario } from './../../../models/usuario.models';
+import { UsuarioService } from './../../../services/usuario.service';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { TipoDeMensagem } from 'src/app/emuns.enum';
 
 @Component({
   selector: 'app-usuario-form',
@@ -7,7 +11,26 @@ import { Location } from '@angular/common';
   styleUrls: ['./usuario-form.component.css']
 })
 export class UsuarioFormComponent implements OnInit {
-  constructor(private _location: Location) {}
+  constructor(
+    private _location: Location,
+    private usuarioService: UsuarioService,
+    private mensagemService: MensagemService
+  ) {}
+
+  usuario = new Usuario();
+
+  salvar(usuario: Usuario) {
+    this.usuarioService.salvar(usuario).subscribe(
+      () => {
+        this.mensagemService.avisoToast('Usuário cadastrado com sucesso!', TipoDeMensagem.Sucesso);
+        this.voltar();
+      },
+      error => {
+        const { mensagem } = error.error;
+        this.mensagemService.avisoToast(mensagem, TipoDeMensagem.Erro);
+      }
+    );
+  }
 
   voltar() {
     this._location.back();
